1e4d3037c23199802a4ded05c1351e4cee2adb43
Chua Chong Herng <chong_herng@hotmail.com>
2020-11-17 19:09:19 +0800

    Added navbar-mobile feature

--- WebClient/js/script.js
+++ WebClient/js/script.js
@@ -1,37 +1,62 @@
-var page = ["#home", "#about", "#products", "#help"];
+var page = ["#home", "#about", "#products", "#help", "#login", "#register"];
+
 var curPage = page[0];
 
-$(document).ready(function () {
-  var newPage = getPage(window.location.hash);
-  render(newPage);
+$(document).ready(function(){
+   var newPage = getPage(window.location.hash);
+   render(newPage);
 
-  $("div a").click(function (e) {
-    e.preventDefault();
+   addActiveClass();
 
-    //Adds active class to clicked link
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active", "");
-    this.className += " active";
+   $('nav a').click(function(e){
+       e.preventDefault();
+       var newPage = $(this).attr('href');
+       window.location.hash=newPage;
+   });
+   
 
-    var newPage = $(this).attr("href");
-    window.location.hash = newPage;
-  });
+   $(window).on('hashchange', function(){
+       var newPage = getPage(window.location.hash);
+       render(newPage);
+   });
 
-  $(window).on("hashchange", function () {
-    var newPage = getPage(window.location.hash);
-    render(newPage);
-  });
+   //mobile nav bar animation
+   $(".toggle").on("click", function(){
+    if($(".nav-link").hasClass("toggled")){
+      $(".nav-link").removeClass("toggled");
+    } else{
+      $(".nav-link").addClass("toggled");
+    }
+   }) 
+
 });
 
-function render(newPage) {
-  if (newPage == curPage) return;
-  $(curPage).hide();
-  $(newPage).show();
-  curPage = newPage;
+
+function render(newPage){
+    if (newPage == curPage) return;
+    $(curPage).hide();
+    $(newPage).show();
+    curPage = newPage; 
 }
 
-function getPage(hash) {
-  var i = page.indexOf(hash);
-  if (i < 0 && hash != "") window.location.hash = page[0];
-  return i < 1 ? page[0] : page[i];
+function getPage(hash){
+   var i = page.indexOf(hash);
+   if (i<0 && hash != "") window.location.hash=page[0]; 
+   return i < 1 ? page[0] : page[i];
 }
+
+function addActiveClass(){
+  var navLinks = document.getElementsByClassName("nav-link");
+  for(var i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+
+      if(current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+
+      this.className += " active";
+    }
+    )}
+}
+
