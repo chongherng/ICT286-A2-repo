8ddf3130f81ca4a9075788700e9437b6e361fbdf
Nullenergien <74352077+Nullenergien@users.noreply.github.com>
2020-11-18 01:23:04 +0800

    Add files via upload

---
+++ Server/query.php
@@ -0,0 +1,35 @@
+<?php
+queryProduct($query);
+
+
+    function queryProduct($query)
+    {
+        $local = 'localhost';
+        $username = 'X33896239';
+        $password = 'X33896239';
+        $dbname = 'X33896239';
+
+        $dbc = mysqli_connect($local, $username, $password, $dbname);
+
+        if (mysqli_connect_errno()) {
+            die("Failed to connect to MySQL: " . mysqli_connect_error() . "<br/>Error number:" . mysqli_connect_errno());
+        }
+        $query = $dbc->real_escape_string($_POST["query"]);
+        $sql = "SELECT * FROM Product WHERE Description LIKE '$query'" ;
+        $result = mysqli_query($dbc, $sql);
+        $dataObjArr = [];
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $dataObj = (object) ['ID' => $row["ProductID"], 'Name' => $row["ProductName"], 'Description' => $row["Description"], 'Price' => "$" . $row["Price"], "ImgSrc" => $row["ImgSrc"]];
+                array_push($dataObjArr, $dataObj);
+            }
+        } else {
+            echo "Error: No result found";
+        }
+
+        $dataJSON = json_encode($dataObjArr);
+        echo $dataJSON;
+        $dbc->close();
+    }
+
+?>
